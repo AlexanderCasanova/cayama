cd8b913c0b7fd5208da079ba90d17392c5b1eb75
AlexanderCasanova <alexandercasanova2000@outlook.com>
2023-07-20 16:58:34 -0500

    navbar mobil avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -18,7 +19,8 @@ import { NosotrosComponent } from './components/nosotros/nosotros.component';
     NosotrosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
+import { style, transition, trigger, animate, state } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('inOutTrigger', [
+      state('out',style({marginTop: '-110%'})),
+      state('in',style({marginTop: '0%'})),
+      transition('in => out', [
+        animate('800ms ease-in')
+      ]),
+      transition('out => in', [
+        animate('800ms ease-out')
+      ])
+    ])
+  ]
 })
 export class NavbarComponent {
+  navstate = 'out';
 
+  toggleNavbar() {
+    this.navstate = (this.navstate==='in')?'out':'in';
+  }
 }
