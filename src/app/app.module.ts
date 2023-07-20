5fd6adb7be01524ce7af2b00c8a6e34fe85116b4
AlexanderCasanova <alexandercasanova2000@outlook.com>
2023-07-20 11:04:01 -0500

    Baseline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServiciosComponent,
+    ContactanosComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule
